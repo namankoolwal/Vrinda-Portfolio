bb8488960be061df61509e6f6c288f9ff29a2c5d
Naman khandelwal <namankoolwal1411@gmail.com>
2024-04-24 11:17:54 +0530

    enhanced for vercel

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import Aboutme from "./Aboutme";
 import StartButton from './assets/images/StartButton.png';
 import Header from "./Header";
@@ -16,12 +16,12 @@ function Homepage() {
     <div className="bg-[url('./assets/images/Background.png')] bg-cover bg-center bg-no-repeat min-h-screen flex justify-center items-center">
       <div className="flex flex-col items-center justify-center">
         <img src={StartButton} alt="Telephone" className="h-auto animate-ring mb-0" />
-        <button
+        <NavLink to='/case-study2'
           className="px-[30px] py-7 ml-10 mt-[20px] text-5xl text-gray-600 font-normal bg-transparent rounded-full hover:text-white hover:bg-black transition-colors duration-500 outline outline-1"
-          onClick={redirectToAboutMe}
+         
         >
           Wanna Chat?
-        </button>
+        </NavLink>
       </div>
     </div>
   );
