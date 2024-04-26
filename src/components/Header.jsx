97f675fbf368119df4403f5a0d7c0ace5855db28
Naman khandelwal <namankoolwal1411@gmail.com>
2024-04-26 16:44:08 +0530

    Add scroll functionality to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos]);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY;
+    setVisible(currentScrollPos < prevScrollPos);
+    setPrevScrollPos(currentScrollPos);
+  };
+
+  const menuItems = [
+    {
+      id: 1,
+      name: "Home",
+      link: "/case-study2",
+    },
+    {
+      id: 2,
+      name: "Projects",
+      link: "/projects",
+    },
+    {
+      id: 3,
+      name: "About Me",
+      link: "/about-me",
+    },
+    {
+      id: 4,
+      name: "Linkedin",
+      link: "/Linkedin",
+    },
+    {
+      id: 5,
+      name: "Resume",
+      link: "/Resume",
+    },
+  ];
+
   return (
     <>
-      <nav className="bg-gray-100 px-4 py-2 md:py-4 md:px-10 sticky top-0 z-50">
+      <nav
+        className={`bg-gray-100 px-4 py-2 md:py-4 md:px-10 sticky top-0 z-50 transition-all duration-500 ${
+          visible ? "top-0" : "-top-28"
+        }`}
+      >
         <button
-        id="hamburger"
+          id="hamburger"
           className="md:hidden block peer/toggle"
           onClick={toggleMenu}
         >
@@ -22,33 +69,35 @@ function Header() {
             <span className="text-2xl">&#9776;</span>
           )}
         </button>
-        <div className={` ${isMenuOpen ? "peer-[#hamburger]/toggle:flex " : "hidden"} pt-2 md:mt-0 hidden md:flex flex-col md:flex-row md:justify-end md:items-center gap-6 `}>
-          <NavLink
-            to="/case-study2"
+        <div
+          className={` ${
+            isMenuOpen ? "peer-[#hamburger]/toggle:flex " : "hidden"
+          } pt-2 md:mt-0 hidden md:flex flex-col md:flex-row md:justify-end md:items-center gap-6 `}
+        >
+          {menuItems.map((item) => (
+            <NavLink
+              key={item.id}
+              to={item.link}
+              className="nav-item"
+              style={({ isActive }) => ({
+                textDecoration: isActive ? "underline" : null,
+              })}
+            >
+              {item.name}
+            </NavLink>
+          ))}
+
+          {/* <NavLink
+            to="/Resume"
             className="nav-item"
-            activeClassName="underline"
+            style={({ isActive }) => ({
+              textDecoration: isActive ? "underline" : null,
+            })}
           >
-            Home
-          </NavLink>
-          <a href="#" className="nav-item">
-            Projects
-          </a>
-          <NavLink
-            to="/about-me"
-            className="nav-item"
-            activeClassName="underline"
-          >
-            About Me
-          </NavLink>
-          <a href="#" className="nav-item">
-            Linkedin
-          </a>
-          <a href="#" className="nav-item">
             Resume
-          </a>
+          </NavLink> */}
         </div>
       </nav>
-      
     </>
   );
 }
