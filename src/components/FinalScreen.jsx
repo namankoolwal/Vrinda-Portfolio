9b1d1cf7a81e6d884854c228ece08aed844f890f
Naman khandelwal <namankoolwal1411@gmail.com>
2024-04-26 15:44:27 +0530

    miner fixes

--- src/components/FinalScreen.jsx
+++ src/components/FinalScreen.jsx
@@ -4,11 +4,12 @@ import eventDiscovery from "../assets/images/eventDiscovery.png";
 import eventCreation from "../assets/images/eventCreation.png";
 import userProfile from "../assets/images/userProfile.png";
 import joinCommunity from "../assets/images/joinCommunity.png";
+import finalStarting from "../assets/images/finalStarting.png";
 import { useState } from "react";
 
 const FinalScreen = () => {
-  const [displayImg, setDisplayImg] = useState(eventDiscovery);
-  const [activeButton, setActiveButton] = useState(1);
+  const [displayImg, setDisplayImg] = useState(finalStarting);
+  const [activeButton, setActiveButton] = useState(0);
 
   const handleButtonClick = (item) => {
     setDisplayImg(item.img);
@@ -37,7 +38,7 @@ const FinalScreen = () => {
     {
       id: 4,
       main: "Join Community",
-      sub: "personalized hub for users, offers a snapshot of their event activity",
+      sub: "connect with others and share event experiences within a community.",
       img: joinCommunity,
     },
   ];
@@ -46,14 +47,14 @@ const FinalScreen = () => {
       <div className="bg-[#211c1c] mb-16">
         <span className="flex items-center gap-2 px-6 py-4 md:py-8">
           <img loading="lazy" src={ellipse}></img>{" "}
-          <span className="font-semibold text-white">Final Screens</span>
+          <span className="font-[500] text-white text-[24px]">Final Screens</span>
         </span>
         <div className="section1 flex flex-col md:flex-row gap-5 md:gap-0 items-center justify-center md:justify-between p-7 md:p-12">
-          <div className="button w-full flex flex-row flex-wrap md:flex-col order-2 md:order-1 justify-center gap-5 md:pl-5">
+          <div className="button w-full flex flex-row flex-wrap md:flex-col order-2 md:order-1 justify-center gap-6 md:pl-5">
             {buttonDetails.map((item) => (
               <button
                 key={item.id}
-                className= {`hover:bg-[#626262c4] pl-2 text-left flex items-center w-full md:w-2/3 p-3 justify-center rounded-md border border-[#929292c4] cursor-pointer ${activeButton === item.id ? 'bg-[#626262c4]' : ''}`}
+                className= {`hover:bg-[#626262c4] pl-2 text-left flex items-center w-full md:w-2/3 h-28 p-3 justify-center rounded-md border border-[#929292c4] cursor-pointer ${activeButton === item.id ? 'bg-[#626262c4]' : ''}`}
                 
                 onClick={() => handleButtonClick(item)}
               >
