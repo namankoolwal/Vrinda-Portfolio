85adf52fe77412fed99635b129a98968213a760e
Naman khandelwal <namankoolwal1411@gmail.com>
2024-05-06 09:10:51 +0530

    Refactor routing paths in App.jsx and LandingPage.jsx components

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,15 @@ import LandingPage from './components/LandingPage';
 import wholeBackground from './assets/images/Background.png';
 // import curserIcon from './assets/Imp/curserIcon.cur';
 function App() {
-  const scrollToRef = useRef(null);
-  const [scrollDone, setscrollDone] = useState(false)
 
   return (
     <div className={`  font-Urbanist  `}>
 
      <Routes>
        <Route exact path="/" element ={<Homepage/>}/>
-       <Route exact path='/landingPage' element={<LandingPage scrollToRef={scrollToRef} setscrollDone={setscrollDone} scrollDone={scrollDone}/>}/>
-       <Route exact path="/about-me" element={<Aboutme scrollToRef={scrollToRef}/>}/>
-       <Route exact path="/case-study2" element={<CaseStudy2 scrollToRef={scrollToRef}/>}/>
+       <Route exact path='/landingPage' element={<LandingPage />}/>
+       <Route exact path="/about-me" element={<Aboutme />}/>
+       <Route exact path="/case-study2" element={<CaseStudy2 />}/>
        <Route exact path="/resume" element={<Resume/>}/>
          
        </Routes>
