001a94b20d041bde39ef8389b0440104255325c2
Naman khandelwal <namankoolwal1411@gmail.com>
2024-05-13 13:55:43 +0530

    feat: Add typed.js library for animated text effect on LandingPage

--- src/components/TypewriterText.jsx
+++ src/components/TypewriterText.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import Typed from 'typed.js';
 
 const TypewriterText = () => {
+    const textRef = useRef(null)
+    useEffect(()=>{
+        const typed = new Typed(textRef.current, {
+            strings: ['designed by me', 'developed by Naman'],
+            typeSpeed: 80,
+            backSpeed: 80,
+            loop: true,
+            cursorChar: '',
+          });
+          return () => {
+            // Destroy Typed instance during cleanup to stop animation
+            typed.destroy();
+          };
+        
+    }, [])
   return (
     <>
-        <div className="wrapper inline-flex">
+        {/* <div className="wrapper inline-flex">
             <div className="static-text">This Website is</div>
             <ul className="dynamic-text overflow-hidden *:relative *:top-0 *:animate-slide ml-1 h-6 leading-6 ">
                 <li ><span className='relative after:absolute after:content-[""]  after:border-red-400 after:h-full after:py-5 after:w-full after:left-0 after:animate-typing after:bg-white'>designed by me</span></li>
                 <li ><span className='relative after:absolute after:content-[""] after:border-red-400 after:h-full after:py-5 after:w-full after:left-0 after:animate-typing after:bg-white'>developed by Naman</span></li>
             </ul>
+        </div> */}
+
+        <div className='wrapper'>
+        <div className="static-text">This Website is <span className='auto-type' ref={textRef}></span></div>
         </div>
     </>
   )
