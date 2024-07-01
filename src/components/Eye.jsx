7b78b2c1165434b6ee65f3562b2f1d8e33421ae9
Naman khandelwal <namankoolwal1411@gmail.com>
2024-07-01 14:32:47 +0530

    eye change

--- src/components/Eye.jsx
+++ src/components/Eye.jsx
@@ -2,57 +2,147 @@ import React, { useState, useEffect } from "react";
 import eyeballs from "../assets/images/landingPage/eyeballs.png";
 
 const Eye = () => {
-  const [eyePosition, setEyePosition] = useState({ x: 0, y: 0 });
+  const [translate, setTranslate] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    const onMouseMove = (e) => {
-      setEyePosition({ x: e.clientX, y: e.clientY });
+    const handleMouseMove = (e) => {
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+
+      const deltaX = mouseX - window.innerWidth / 2;
+      const deltaY = mouseY - window.innerHeight / 2;
+
+      // Set the translation values based on mouse position
+      setTranslate({ x: deltaX * 0.05, y: deltaY * 0.04 });
     };
 
-    document.addEventListener("mousemove", onMouseMove);
-
+    window.addEventListener("mousemove", handleMouseMove);
     return () => {
-      document.removeEventListener("mousemove", onMouseMove);
+      window.removeEventListener("mousemove", handleMouseMove);
     };
   }, []);
 
- 
-
-  const eyeBallStyle = {
-    position: "absolute",
-   
-    transform: `translate(${eyePosition.x / 20}px, ${eyePosition.y / 20}px) rotate(${-45}deg) `, 
-  };
- 
-
-// more css is imported from App.css  
-
   return (
     <>
-      <div className="flex md:gap-28 gap-20 ">
-        <div  className=" eye flex items-center justify-center md:block w-[80px] h-[80px] md:w-[130px] md:h-[130px]">
-        <div className="eyelid">
-                <span></span>
+      <div className="flex relative md:gap-28 gap-20">
+        <div className="relative">
+          <div className="eye relative flex items-center justify-center md:block w-[80px] h-[80px] md:w-[130px] md:h-[130px]">
+            <div className="eyelid">
+              <span className=""></span>
             </div>
-          <div  style={eyeBallStyle} className="w-[60px] h-[60px] md:w-[80px] md:h-[80px]  animate-eyeball md:animate-none">
-            <img src={eyeballs} alt="" />
+            <div className="relative w-full h-full -rotate-45 flex items-center justify-center">
+              <div
+                style={{
+                  transform: `translate(${translate.x}px, ${translate.y}px)`,
+                  transition: "transform 0.1s easein",
+                }}
+                className="w-[60px] h-[60px] md:w-[80px] md:h-[80px] bg-black rounded-full animate-eyeball md:animate-none"
+              ></div>
+            </div>
           </div>
+
+        
         </div>
 
-        <div  className="eye flex items-center justify-center md:block w-[80px] h-[80px] md:w-[130px] md:h-[130px] ">
-        <div className="eyelid ">
-                <span></span>
+        <div className="relative">
+          <div className="eyeright relative flex items-center justify-center md:block w-[80px] h-[80px] md:w-[130px] md:h-[130px]">
+            <div className="relative w-full h-full -rotate-45 flex items-center justify-center">
+              <div
+                style={{
+                  transform: `translate(${translate.x}px, ${translate.y}px)`,
+                  transition: "transform 0.1s easein",
+                }}
+                className="w-[55px] h-[55px] md:w-[80px] md:h-[80px] bg-black rounded-full animate-eyeball md:animate-none"
+              ></div>
             </div>
-        <div  style={eyeBallStyle} className="w-[60px] h-[60px]  md:w-[80px] md:h-[80px]  animate-eyeball md:animate-none">
-            <img src={eyeballs} alt="" className=""/>
           </div>
+
+         
         </div>
       </div>
-
-
-
     </>
   );
 };
 
 export default Eye;
+
+
+
+
+
+
+// .eyeright{
+   
+//   border: 4px solid black;
+//   border-radius: 85% 10px;
+//   transform: rotate(45deg);
+//   overflow: hidden;
+// }
+// .eye{
+//   border: 4px solid black;
+//   border-radius: 85% 10px;
+//   transform: rotate(45deg);
+//   overflow: hidden;
+//   height: 130px;
+//   width: 130px;
+//   animation: eyeborder 4s infinite;
+// }
+
+// @keyframes eyeborder {
+//   0%, 10%, 18%, 100% {
+//       border-top: 4px solid black;
+//       border-left: 4px solid black;
+//   }
+//   10%, 15% {
+//       border-top: 4px solid white;
+//       border-left: 4px solid white;
+//   }
+// }
+
+// .eyelid {
+//   width: 200px;
+//   height: 100px;
+//   z-index: 1000;
+//   position: absolute;
+//   top: 50%;
+//   left: 50%;
+//   transform: translate(-50%, -50%) rotate(-45deg);
+// }
+
+// .eyelid span {
+//   display: block;
+//   width: 100%;
+//   height: 0;
+//   background-color: white;
+//   border: 4px solid black;
+//   border-radius: 0 0 60% 60%;
+//   animation: eyelid 4s infinite;
+// }
+
+// @keyframes eyelid {
+//   0%, 20%, 100% {
+//       height: 0;
+//   }
+//   10% {
+//       height: 100%;
+//   }
+// }
+
+// .eyelashed {
+//   position: absolute;
+//   width: 50%;
+//   height: 100%;
+//   left: 0;
+//   top: 0;
+//   display: flex;
+//   justify-content: space-around;
+//   align-items: flex-start;
+// }
+
+// .eyelashed span {
+//   border: 1px solid black;
+//   background-color: black;
+//   width: 2.4px;
+//   height: 4px;
+//   transform: rotate(-12deg);
+// }
--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import eyeballs from "../assets/images/landingPage/eyeballs.png";
 
+import eyeclose from "../assets/images/landingPage/eyeclose.svg";
+import eyeopen from "../assets/images/landingPage/eyeopen.svg";
+
+
 const Eye = () => {
   const [translate, setTranslate] = useState({ x: 0, y: 0 });
 
@@ -26,6 +30,9 @@ const Eye = () => {
     <>
       <div className="flex relative md:gap-28 gap-20">
         <div className="relative">
+        <img src={eyeopen} alt=""  className="lefteye  absolute -top-3 z-10 w-full h-full scale-[1.6] overflow-hidden "/>
+        <img src={eyeclose} className="lefteyeclose absolute top-6 left-2 z-10 w-full h-full scale-[1.5] overflow-hidden "/>
+
           <div className="eye relative flex items-center justify-center md:block w-[80px] h-[80px] md:w-[130px] md:h-[130px]">
             <div className="eyelid">
               <span className=""></span>
@@ -36,50 +43,30 @@ const Eye = () => {
                   transform: `translate(${translate.x}px, ${translate.y}px)`,
                   transition: "transform 0.1s easein",
                 }}
-                className="w-[60px] h-[60px] md:w-[80px] md:h-[80px] bg-black rounded-full animate-eyeball md:animate-none"
-              ></div>
+                className="eyeball w-[60px] h-[60px] md:w-[80px] md:h-[80px] bg-[#fc6d42] rounded-full animate-eyeball md:animate-none"
+              >
+               
+              </div>
             </div>
           </div>
-
-          {/* <div className="eyelashed absolute w-full h-full flex gap-4 flex-nowrap bg-red-200/40 rounded-full">
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-          </div> */}
+        
         </div>
 
         <div className="relative">
+          <img src={eyeopen} alt=""  className="absolute -top-3 z-10 w-full h-full scale-[1.6] overflow-hidden "/>
           <div className="eyeright relative flex items-center justify-center md:block w-[80px] h-[80px] md:w-[130px] md:h-[130px]">
-            <div className="relative w-full h-full -rotate-45 flex items-center justify-center">
+            <div className="relative w-full h-full -rotate-45 flex items-center justify-center ">
               <div
                 style={{
                   transform: `translate(${translate.x}px, ${translate.y}px)`,
                   transition: "transform 0.1s easein",
                 }}
-                className="w-[55px] h-[55px] md:w-[80px] md:h-[80px] bg-black rounded-full animate-eyeball md:animate-none"
+                className="w-[55px] h-[55px] md:w-[80px] md:h-[80px] bg-[#fc6d42] rounded-full animate-eyeball md:animate-none"
               ></div>
             </div>
           </div>
 
-          {/* <div className="eyelashed absolute w-full h-full flex gap-4 flex-nowrap bg-red-200 rounded-full">
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-            <span className="border border-black bg-black w-[2.4px] h-4 -rotate-12"></span>
-          </div> */}
+         
         </div>
       </div>
     </>
@@ -92,79 +79,3 @@ export default Eye;
 
 
 
-
-// .eyeright{
-   
-//   border: 4px solid black;
-//   border-radius: 85% 10px;
-//   transform: rotate(45deg);
-//   overflow: hidden;
-// }
-// .eye{
-//   border: 4px solid black;
-//   border-radius: 85% 10px;
-//   transform: rotate(45deg);
-//   overflow: hidden;
-//   height: 130px;
-//   width: 130px;
-//   animation: eyeborder 4s infinite;
-// }
-
-// @keyframes eyeborder {
-//   0%, 10%, 18%, 100% {
-//       border-top: 4px solid black;
-//       border-left: 4px solid black;
-//   }
-//   10%, 15% {
-//       border-top: 4px solid white;
-//       border-left: 4px solid white;
-//   }
-// }
-
-// .eyelid {
-//   width: 200px;
-//   height: 100px;
-//   z-index: 1000;
-//   position: absolute;
-//   top: 50%;
-//   left: 50%;
-//   transform: translate(-50%, -50%) rotate(-45deg);
-// }
-
-// .eyelid span {
-//   display: block;
-//   width: 100%;
-//   height: 0;
-//   background-color: white;
-//   border: 4px solid black;
-//   border-radius: 0 0 60% 60%;
-//   animation: eyelid 4s infinite;
-// }
-
-// @keyframes eyelid {
-//   0%, 20%, 100% {
-//       height: 0;
-//   }
-//   10% {
-//       height: 100%;
-//   }
-// }
-
-// .eyelashed {
-//   position: absolute;
-//   width: 50%;
-//   height: 100%;
-//   left: 0;
-//   top: 0;
-//   display: flex;
-//   justify-content: space-around;
-//   align-items: flex-start;
-// }
-
-// .eyelashed span {
-//   border: 1px solid black;
-//   background-color: black;
-//   width: 2.4px;
-//   height: 4px;
-//   transform: rotate(-12deg);
-// }
